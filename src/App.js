96a2e473f6a189dcb0936d84e2eddef8311b4091
VIVES-ELICT <hans.naert@vives.be>
2022-05-12 01:11:06 +0200

    fetch added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  const [fetchInfo, setFetchInfo] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setFetchInfo(data);
+      });
+  });
+  function HelloWorld() {
+    console.log('Hello World');
+    setFetchInfo('Hello World');
+  }
+  return (
+    <div>
+      <Button variant="contained" onClick={HelloWorld}>
+        Hello World
+      </Button>
+      <div>
+        {JSON.stringify(fetchInfo)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
